3d0137ab184d4d65a4012b4c620a18a97f7d998a
CJ-404 <cj.ptsz@yahoo.com>
2024-12-25 06:55:40 +0530

    fix: email js service

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -31,9 +31,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Charith Jayarangana",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "charithjayarangana3@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
