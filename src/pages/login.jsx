d131288d4dbbaaef92b7c0edac7e602cec90c3b0
CJ-404 <cj.ptsz@yahoo.com>
2024-12-25 07:29:56 +0530

    feat: not found page

---
+++ src/pages/login.jsx
@@ -0,0 +1,85 @@
+import emailjs from "@emailjs/browser";
+import { Canvas } from "@react-three/fiber";
+import { Suspense, useRef, useState } from "react";
+
+import { Fox } from "../models";
+import useAlert from "../hooks/useAlert";
+import { Alert, Loader } from "../components";
+
+const Login = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: "", email: "", message: "" });
+  const { alert, showAlert, hideAlert } = useAlert();
+  const [loading, setLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState("idle");
+
+  const handleChange = ({ target: { name, value } }) => {
+    setForm({ ...form, [name]: value });
+  };
+
+  const handleFocus = () => setCurrentAnimation("walk");
+  const handleBlur = () => setCurrentAnimation("idle");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    setCurrentAnimation("hit");
+
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: "Charith Jayarangana",
+          from_email: form.email,
+          to_email: "charithjayarangana3@gmail.com",
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(
+        () => {
+          setLoading(false);
+          showAlert({
+            show: true,
+            text: "Thank you for your message 😃",
+            type: "success",
+          });
+
+          setTimeout(() => {
+            hideAlert(false);
+            setCurrentAnimation("idle");
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          }, [3000]);
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+          setCurrentAnimation("idle");
+
+          showAlert({
+            show: true,
+            text: "I didn't receive your message 😢",
+            type: "danger",
+          });
+        }
+      );
+  };
+
+  return (
+    <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert {...alert} />}
+
+      <div className='flex-1 min-w-[50%] flex flex-col'>
+        <h1 className='head-text'>Login</h1>
+      </div>
+    </section>
+  );
+};
+
+export default Login;
