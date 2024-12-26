a4a19b5a664c84495bdc97ea579b2332e46445a6
CJ-404 <cj.ptsz@yahoo.com>
2024-12-26 20:08:36 +0530

    feat: add profile picture in home. fix: download cv button

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,17 +1,28 @@
 import { Link } from "react-router-dom";
-
+import { profile } from "../assets/images";
 import { arrow } from "../assets/icons";
 
 const HomeInfo = ({ currentStage }) => {
   if (currentStage === 1)
     return (
-      <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-        Hi, I'm
-        <span className='font-semibold mx-2 text-white'>Charith</span>
-        👋
-        <br />
-        A Computer Science 4th year Undergraduate from UCSC
-      </h1>
+      <div className='flex items-center justify-center'>
+        <div className='sm:text-xl sm:leading-snug text-center py-4 px-8 text-black flex flex-col items-center'>
+          <img
+            draggable='false'
+            src={profile}
+            alt="Profile Photo"
+            className='w-32 h-32 rounded-full mb-4'
+          />
+          <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+            Hi, I'm
+            <span className='font-semibold mx-2 text-white'>Charith</span>
+            👋
+            <br />
+            Computer Science 4th year Undergraduate from UCSC
+          </h1>
+        </div>
+      </div>
+
     );
 
   if (currentStage === 2) {
